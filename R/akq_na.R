6fc93d36acabc06b357a314541449173eac450e5
wasquith-usgs <wasquith@usgs.gov>
2018-07-12 09:25:39 -0500

    added support for testing if the fill_* returned a "list()" (empty list object), this was added for the mnget() testing for a colleague

--- R/akq_na.R
+++ R/akq_na.R
@@ -1,6 +1,6 @@
 "akq_na" <- function(akqenv, silent=FALSE, ...) {
   if(is.null(akqenv)) {
-     warning(" need to specify the akqenv [fill_akqenv()] or daily-value [fill_dveqn()] environment")
+     warning(" need to specify the akqenv [fill_akqenv()], daily-value [fill_dvenv()], or monthly-value [fill_mnenv()] environment")
      return()
   } else if(! is.environment(akqenv)) {
      warning(" akqenv is not an environment")
@@ -20,14 +20,18 @@
        if(length(Z[,1]) == 0) {
          t <- t + 1; NASITES[t] <- SITES[i]; next
        }
+    } else if(is.list(Z)) {
+       if(length(Z) == 0) {
+         t <- t + 1; NASITES[t] <- SITES[i]; next
+       }
     }
   }
   m <- length(NASITES)
   if(! silent & m == 0) {
-     message(" Found zero sites with NA akdecay() processing or zero rows of daily values dvget().")
+     message(" Found zero sites with NA akdecay() processing, zero rows of daily values dvget(), or zero rows of monthly values mnget().")
   } else if(! silent) {
      message(" Found ", length(NASITES),
-             " having NA akdecay() processing or zero rows of daily values dvget()\n",
+             " having NA akdecay() processing, zero rows of daily values dvget(), or zero rows of monthly values mnget()\n",
              "    Consider running akq_rm(akqenv, THE_SITES) on these sites, and\n",
              "    don't forget to update your master site list though!")
   }
