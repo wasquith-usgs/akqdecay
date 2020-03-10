3a71e68d8e997417438092aac99ebbf146300b8d
William Asquith <wasquith@usgs.gov>
2020-03-10 13:57:13 -0500

    Update akq_plotfdc.R

--- R/akq_plotfdc.R
+++ R/akq_plotfdc.R
@@ -28,7 +28,7 @@ function(gagefdc, site="", file=NA, showtitle=TRUE, ylim=NULL, ...) {
     lmomco::add.log.axis(logs=c(1, 2, 4, 6), side=2, make.labs=TRUE, las=1, label="")
     lmomco::add.lmomco.axis(las=2,  tcl=0.5, side.type="NPP", cex=0.8, case="lower")
 
-    ks <- seq(0.4,0.2*length(decades)+0.4, by=0.2)-0.1
+    ks <- seq(0.3,0.15*length(decades)+0.3, by=0.15)
     cols <- rev(ks/max(ks)) - 0.05
     cols[cols > 0.85] <- 0.85
     cols[cols < 0.10] <- 0.10
@@ -40,14 +40,20 @@ function(gagefdc, site="", file=NA, showtitle=TRUE, ylim=NULL, ...) {
       points(ff, gmp, pch=21, bg=grey(cols[i]), lwd=0.7, cex=ks[i])
     }
 
-    txt <- c(paste0("Decadal FDC where circle size and greyness increases\n",
-                    "by decade from", decades[       1       ],"s to ",
-                                      decades[length(decades)],"s,",
-                    "where dashed lines, if present,\n",
-                    "indicate 1950 decade and earlier"))
+    legend(2,10^mean(par()$usr[3:4]),
+                  paste(decades,"s", sep=""), cex=0.7, bty="n", pch=21,
+                  pt.cex=ks, lty=0, pt.bg=grey(cols),
+                  title="Flow-duration\ncurve (FDC)\nquantile")
+
+
+    txt <- c(paste0("Decadal FDC with circle size and greyness increasing\n",
+                    "by decade from ", decades[       1       ],"s to ",
+                                       decades[length(decades)],"s,",
+                    "where dashed lines,\n",
+                    "if present, indicate 1950 decade and earlier"))
     legend(-3.3, 10^(par()$usr[4] - 0.05*(diff(par()$usr[3:4]))), txt,
            pt.bg=grey(0.7), bty="n", lwd=0.7, pch=21, cex=0.8, pt.cex=0.9)
-    if(showtitle) mtext(paste0("DECADAL FLOW-DURATION CURVES STREAMGAGE: ",site))
+    if(showtitle) mtext(paste0("STREAMGAGE: ",site))
     par(opts)
   if(! is.na(file)) dev.off()
 }
@@ -56,4 +62,4 @@ function(gagefdc, site="", file=NA, showtitle=TRUE, ylim=NULL, ...) {
 #dv   <- dvget(site, ignore.provisional=FALSE)
 #akq  <- akqdecay(dv)
 #gage  <- fdclmr(dv, decade=TRUE)
-#akq_plotfdc2(gage, site=site)
+#akq_plotfdc(gage, site=site)
