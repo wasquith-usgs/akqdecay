8f918b80e55807e43613a95f6f1849a241c27fb6
wasquith-usgs <wasquith@usgs.gov>
2020-03-10 11:18:14 -0500

    updated

--- R/akq_plotfdc.R
+++ R/akq_plotfdc.R
@@ -1,5 +1,5 @@
 "akq_plotfdc" <-
-function(gagefdc, site="", file=NA, showtitle=TRUE, ...) {
+function(gagefdc, site="", file=NA, showtitle=TRUE, ylim=NULL, ...) {
   fdc  <- as.data.frame(gagefdc[,c(7:33)])        # extracting the "f50" (median) etc column names
   ff   <- as.numeric(gsub("f","",names(fdc)))/100 # stripping the leading "f" and converted from percent
   ff   <- qnorm(ff)                               # transformation to standard normal variates
@@ -10,19 +10,23 @@ function(gagefdc, site="", file=NA, showtitle=TRUE, ...) {
   }
   decades <- names(fdc)
 
-  ylim <- range(fdc)
-  ylim <- c(0.01, 10^ceiling(log10(ylim[2])))
+  if(is.null(ylim)) {
+    ylim <- range(fdc)
+    flr <- 10^(floor(log10(ylim[1]))-1)
+    flr <- ifelse(flr == 0, 0.01, flr)
+    ylim <- c(flr, 10^ceiling(log10(ylim[2]))); rm(flr)
+  }
 
   if(! is.na(file)) pdf(file, useDingbats=FALSE, height=6, width=7)
-    opts <- par(); par(las=1, mgp=c(3,0.5,0))
+    opts <- par(no.readonly=TRUE); par(las=1, mgp=c(3,0.5,0))
 
     plot(1,1, xlim=qnorm(c(0.0001, 0.9999)), ylim=ylim,
               xaxt="n", yaxt="n", xlab="", log="y", type="n",
               xaxs="i", yaxs="i", ylab="Streamflow, cfs")
-    lmomco::add.lmomco.axis(las=2,  tcl=0.5, side.type="NPP", cex=0.8, case="lower")
     lmomco::add.log.axis(side=2,    tcl=0.8*abs(par()$tcl), two.sided=TRUE)
     lmomco::add.log.axis(logs=c(1), tcl=+1.3*abs(par()$tcl), side=2, two.sided=TRUE)
     lmomco::add.log.axis(logs=c(1, 2, 4, 6), side=2, make.labs=TRUE, las=1, label="")
+    lmomco::add.lmomco.axis(las=2,  tcl=0.5, side.type="NPP", cex=0.8, case="lower")
 
     ks <- seq(0.4,0.2*length(decades)+0.4, by=0.2)-0.1
     cols <- rev(ks/max(ks)) - 0.05
@@ -44,7 +48,7 @@ function(gagefdc, site="", file=NA, showtitle=TRUE, ...) {
     legend(-3.3, 10^(par()$usr[4] - 0.05*(diff(par()$usr[3:4]))), txt,
            pt.bg=grey(0.7), bty="n", lwd=0.7, pch=21, cex=0.8, pt.cex=0.9)
     if(showtitle) mtext(paste0("DECADAL FLOW-DURATION CURVES STREAMGAGE: ",site))
-    suppressWarnings(par(opts))
+    par(opts)
   if(! is.na(file)) dev.off()
 }
 
@@ -52,4 +56,4 @@ function(gagefdc, site="", file=NA, showtitle=TRUE, ...) {
 #dv   <- dvget(site, ignore.provisional=FALSE)
 #akq  <- akqdecay(dv)
 #gage  <- fdclmr(dv, decade=TRUE)
-#akq_plotfdc(gage)
+#akq_plotfdc2(gage, site=site)
