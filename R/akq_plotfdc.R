a01cd57e4aa2845eec07f9c87e8adb0fe0e43f54
William Asquith <wasquith@usgs.gov>
2020-03-13 14:11:18 -0500

    syntax fix

--- R/akq_plotfdc.R
+++ R/akq_plotfdc.R
@@ -5,11 +5,15 @@ function(gagefdc, site="", file=NA, rev.decades=TRUE,
     message("ALERT: empty 'gagefdc' for site=",site)
     return(NULL)
   }
-  fdc  <- as.data.frame(gagefdc[,c(7:33)])        # extracting the "f50" (median) etc column names
-  ff   <- as.numeric(gsub("f","",names(fdc)))/100 # stripping the leading "f" and converted from percent
-  ff   <- qnorm(ff)                               # transformation to standard normal variates
+  fdc  <- as.data.frame(gagefdc[,c(7:33)])        # extracting the "f50"
+                                                  # (median) etc column names
+  ff   <- as.numeric(gsub("f","",names(fdc)))/100 # stripping the leading "f"
+                                                  # and converted from percent
+  ff   <- qnorm(ff)                               # transformation to standard
+                                                  # normal variates
   fdc  <- as.data.frame(t(as.matrix(fdc)))        # some data manipulation
-  names(fdc) <- as.character(gagefdc$decade) # and finally naming the flow-durations curves by decade
+  names(fdc) <- as.character(gagefdc$decade) # and finally naming the flow-
+                                             # duration curves by decade
   for(i in rev(1:length(fdc[1,]))) {
     if(is.na(fdc[1,i])) fdc[,i] <- NULL
   }
@@ -50,9 +54,9 @@ function(gagefdc, site="", file=NA, rev.decades=TRUE,
     tcl <- abs(par()$tcl)
     lmomco::add.log.axis(side=2,    tcl= 0.8*tcl, two.sided=TRUE)
     lmomco::add.log.axis(logs=c(1), tcl=+1.3*tcl, two.sided=TRUE, side=2)
-    lmomco::add.log.axis(logs=c(1, 2, 4, 6), side=2, make.labs=TRUE, las=1, label="")
-    lmomco::add.lmomco.axis(las=2,  tcl=0.5, side.type="NPP", cex=0.8, case="lower",
-                            twoside=TRUE, twoside.suppress.labels=TRUE)
+    lmomco::add.log.axis(logs=c(1,2,4,6), side=2, make.labs=TRUE, las=1, label="")
+    lmomco::add.lmomco.axis(las=2,  tcl=0.5, side.type="NPP", cex=0.8,
+                       case="lower", twoside=TRUE, twoside.suppress.labels=TRUE)
 
     ks <- seq(0.3,0.15*length(decades)+0.3, by=0.15)
     cols <- rev(ks/max(ks)) - 0.05
@@ -68,20 +72,20 @@ function(gagefdc, site="", file=NA, rev.decades=TRUE,
       points(ff, gmp, pch=21, bg=grey(cols[i]), lwd=0.7, cex=ks[i])
     }
 
-    legend(qnorm(0.95),10^mean(par()$usr[3:4]),
+    legend(qnorm(0.95), 10^mean(par()$usr[3:4]),
                   paste(decades,"s", sep=""), cex=0.7, bty="n", pch=21,
-                  pt.cex=ks, lty=0, pt.bg=grey(cols), ncol=2,
+                  pt.cex=ks, lty=0, pt.bg=grey(cols), ncol=2, lwd=0.7,
                   title="Flow-duration\ncurve (FDC)\nquantile")
 
 
     txt <- c(paste0("Decadal FDC with circle size and greyness increasing\n",
                     "by decade from ", decades[       1       ],"s to ",
-                                       decades[length(decades)],"s,",
+                                       decades[length(decades)],"s, ",
                     "where dashed lines,\n",
                     "if present, indicate 1950 decade and earlier"))
     legend(-3.3, 10^(par()$usr[4] - 0.05*(diff(par()$usr[3:4]))), txt,
            pt.bg=grey(0.7), bty="n", lwd=0.7, pch=21, cex=0.7, pt.cex=0.9)
-    if(showtitle) mtext(paste0("STREAMGAGE: ",site))
+    if(showtitle) mtext(paste0("STREAMGAGE: ", site))
     par(opts)
   if(! is.na(file)) dev.off()
   }
